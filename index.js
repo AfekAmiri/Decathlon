a1999be577ad96c5dc62a5a9e6a5bba078500799
mbaus24 <baus.martin@yahoo.fr>
2023-10-17 16:32:55 +0200

    npm init

--- index.js
+++ index.js
@@ -1,73 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lancée de poids</title>
+import * as THREE from 'three';
 
-    <!-- Bootstrap CSS (Assurez-vous de télécharger le fichier bootstrap.min.css et de l'inclure dans votre projet) -->
-    <link rel="stylesheet" href="chemin/vers/bootstrap.min.css">
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    <style>
-        /* Ajoutez vos styles CSS ici */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f0f0f0;
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 1em;
-        }
-
-        section {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        footer {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 1em;
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>
-</head>
-<body>
-
-    <header>
-        <h1>Lancée de poids</h1>
-    </header>
-
-    <section>
-        <h2>Commencez votre aventure ici!</h2>
-        <h2>Règles du jeu</h2>
-
-        <!-- Ajoutez vos éléments de jeu ici -->
-
-    </section>
-
-    <!-- Three.js -->
-    <script src="chemin/vers/three.min.js"></script>
-
-    <!-- Votre script JavaScript pour le jeu -->
-    <script src="chemin/vers/votre-script.js"></script>
-
-    <!-- Bootstrap JavaScript (Assurez-vous de télécharger le fichier bootstrap.min.js et de l'inclure dans votre projet) -->
-    <script src="chemin/vers/bootstrap.min.js"></script>
-
-    <footer>
-        <p>&copy; Shot Put. Tous droits réservés.</p>
-    </footer>
-
-</body>
-</html>
-
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
